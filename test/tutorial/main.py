e88fb1f4d2f29a9300316e70b01c905cf1ba0760
JoshSwain <joshfswain@gmail.com>
2023-09-30 10:13:20 -0400

    "New post and get routes!"

--- test/tutorial/main.py
+++ test/tutorial/main.py
@@ -1,6 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from enum import Enum
+
 
 app = FastAPI()
 
@@ -29,3 +30,49 @@ items = {
 @app.get("/")
 def index() -> dict[str, dict[int, Item]]:
     return {"items": items}
+
+#Routing by Item ID
+@app.get("/items/{item_id}")
+def query_item_by_id(item_id: int) -> Item:
+    if item_id not in items:
+        raise HTTPException(
+            status_code=404, detail=f"Item with ID '{item_id}' was not found"
+        )
+    return items[item_id]
+
+
+Selector = dict[
+    str, str | float | int | Category | None
+] 
+
+@app.get("/items/")
+def query_item_by_param(
+    name: str | None = None, 
+    price: float | None = None, 
+    count: int | None = None, 
+    category: Category | None = None,
+) -> dict[str, Selector | list[Item]]: 
+    def check_item(item: Item) -> bool:
+        return all(
+            (
+                name is None or name==item.name,
+                price is None or price==item.price,
+                count is None or count==item.count,
+                category is None or category is item.category
+            )
+        )
+    selection = [item for item in items.values() if check_item(item)]
+    return {
+        "query": {"name":name, "price": price, "count": count, "category": category},
+        "selection": selection
+    }
+
+@app.post("/")
+def add_item(item: Item) -> dict[str, Item]:
+    
+    if item.id in items:
+        raise HTTPException(
+            status_code=400, detail=f"Item with ID {item.id} already exists"
+        )
+    items[item.id] = item
+    return {"added": item}
--- test/tutorial/test_server.py
+++ test/tutorial/test_server.py
@@ -1,5 +1,17 @@
 import requests
-print(requests.get("http://localhost:4008/").json())
+# print(requests.get("http://localhost:8000/").json())
+
+print(
+    requests.post(
+        "http://127.0.0.1:8000/",
+        json={"name": "Ice Cream Sandwich", "price": 3.99, "count": 10, "id": 4, "category": "sandwiches"},
+    ).json()
+)
+
+
+print(requests.get("http://localhost:8000/items/4").json()
+      )
+
 
 
 

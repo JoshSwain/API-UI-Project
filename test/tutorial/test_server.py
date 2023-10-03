c7f94ef3c51dca9aea9e5da58eb2f7168b6fe9e2
JoshSwain <joshfswain@gmail.com>
2023-10-03 07:25:51 -0400

    Before adding SQL Server

--- test/tutorial/test_server.py
+++ test/tutorial/test_server.py
@@ -1,17 +1,30 @@
 import requests
 # print(requests.get("http://localhost:8000/").json())
 
+#print(requests.get("http://localhost:8000/items?name=Grinder").json())
+
+
+# print(
+#     requests.post(
+#         "http://127.0.0.1:8000/",
+#         json={"name": "Ice Cream Sandwich", "price": 3.99, "count": 10, "id": 4, "category": "sandwiches"},
+#     ).json()
+# )
+
 print(
-    requests.post(
-        "http://127.0.0.1:8000/",
-        json={"name": "Ice Cream Sandwich", "price": 3.99, "count": 10, "id": 4, "category": "sandwiches"},
+    requests.put(
+        "http://127.0.0.1:8000/update/2?category=beverages",
+        
     ).json()
 )
 
 
-print(requests.get("http://localhost:8000/items/4").json()
-      )
+# print(requests.get("http://localhost:8000/items/4").json()
+#       )
 
+# print(requests.delete("http://127.0.0.1:8000/delete/5").json())
 
+# print(requests.get("http://localhost:8000/items/4").json()
+#       )
 
 

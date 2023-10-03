2b15959b4d52ada00de90a3e1a0bf12803218835
JoshSwain <joshfswain@gmail.com>
2023-10-03 07:30:35 -0400

    Updated 2 id

--- test/tutorial/main.py
+++ test/tutorial/main.py
@@ -23,7 +23,7 @@ class Item(BaseModel):
 items = {
     0: Item(name="Grinder", price=9.99, count=20, id=0, category=Category.SANDWICHES),
     1: Item(name="Sausage Egg and Cheese", price=5.99, count=40, id=1, category=Category.SANDWICHES),
-    2: Item(name="Iced Coffee", price=4.99, count=30, id=0, category=Category.BEVERAGES)
+    2: Item(name="Iced Coffee", price=4.99, count=30, id=2, category=Category.BEVERAGES)
 }
 
 # API Routing, get all items
--- test/tutorial/test_server.py
+++ test/tutorial/test_server.py
@@ -1,7 +1,7 @@
 import requests
 # print(requests.get("http://localhost:8000/").json())
 
-#print(requests.get("http://localhost:8000/items?name=Grinder").json())
+print(requests.get("http://localhost:8000/items/2").json())
 
 
 # print(
@@ -11,12 +11,12 @@ import requests
 #     ).json()
 # )
 
-print(
-    requests.put(
-        "http://127.0.0.1:8000/update/2?category=beverages",
+# print(
+#     requests.put(
+#         "http://127.0.0.1:8000/update/2?category=beverages",
         
-    ).json()
-)
+#     ).json()
+# )
 
 
 # print(requests.get("http://localhost:8000/items/4").json()
